abd0f826313e76597bfe03a20dead54b5a421af3
Jimmy Chuang <ogre0403@gmail.com>
2022-01-30 22:35:52 +0800

    bootstrap hdfs and yarn

--- entry.sh
+++ entry.sh
@@ -3,6 +3,29 @@
 
 /usr/sbin/sshd -D &
 
+sleep 10s
+
+DIR="/tmp/hadoop_dir/dfs/name"
+
+
+
+source /root/.bashrc
+
+ssh-keyscan localhost >> /root/.ssh/known_hosts
+ssh-keyscan 0.0.0.0 >> /root/.ssh/known_hosts
+
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "HDFS already formated"
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Format HDFS"
+  hadoop namenode -format
+fi
+
+start-dfs.sh
+start-yarn.sh
+
 python3 /app/xterm.py &
 
 wait -n
